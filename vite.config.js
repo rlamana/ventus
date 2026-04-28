a15f98146f1399a75ebcff30bb613aa510df8366
Ramon Lamana <rlamana@gmail.com>
2026-04-29 00:19:23 +0200

    Preview app with vite

--- vite.config.js
+++ vite.config.js
@@ -2,31 +2,13 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import autoprefixer from 'autoprefixer'
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/ventus.js'),
-      name: 'Ventus',
-      formats: ['es', 'umd'],
-      fileName: (format) => `ventus.${format === 'es' ? 'js' : 'umd.cjs'}`
-    },
-    rollupOptions: {
-      external: [],
-      output: {
-        globals: {},
-        exports: 'named'
-      }
-    },
-    sourcemap: true,
-    minify: 'terser'
-  },
-  
+const sharedConfig = {
   resolve: {
     alias: {
       'ventus': resolve(__dirname, 'src/ventus')
     }
   },
-  
+
   css: {
     preprocessorOptions: {
       less: {
@@ -39,8 +21,40 @@ export default defineConfig({
       ]
     }
   },
-  
+
   test: {
     environment: 'jsdom'
+  }
+}
+
+export default defineConfig(({ mode }) => {
+  if (mode === 'demo') {
+    return {
+      ...sharedConfig,
+      build: {
+        outDir: 'dist-demo'
+      }
+    }
+  }
+
+  return {
+    ...sharedConfig,
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/ventus.js'),
+        name: 'Ventus',
+        formats: ['es', 'umd'],
+        fileName: (format) => `ventus.${format === 'es' ? 'js' : 'umd.cjs'}`
+      },
+      rollupOptions: {
+        external: [],
+        output: {
+          globals: {},
+          exports: 'named'
+        }
+      },
+      sourcemap: true,
+      minify: 'terser'
+    }
   }
 })
